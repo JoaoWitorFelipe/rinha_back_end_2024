4d0741751a60a0975831bec5dfa5592e0e5fdf66
Vinicius Nazario <viniciusnazario25@gmail.com>
2024-02-13 14:05:55 -0300

    feature: implements statement endpoint refactor: connect with database in all script context

--- index.js
+++ index.js
@@ -10,45 +10,104 @@ const client = new pg.Client({
     password: '123',
 })
 
+await client.connect()
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/clientes/:id/transacoes', async ({ body, params }, res) => {
-    await client.connect()
- 
-    const dbRes = await client.query(
-        `
-            INSERT INTO transacoes
-                (
-                    valor,
-                    tipo,
-                    descricao,
-                    cliente_id
-                )
-            VALUES
-                (   
-                    $1,
-                    $2,
-                    $3,
-                    $4
-                )
-        `,
-        [
-            body.valor,
-            body.tipo,
-            body.descricao,
-            params.id,
-        ],
-    );
+    try {
+        const result = await client.query(`SELECT * from clientes where id = $1`, [params.id]);
 
-    console.log(dbRes);
+        if (!result.rowCount) {
+            return res.status(404).send()
+        }
 
-    await client.end()
+        await client.query(
+            `
+                INSERT INTO transacoes
+                    (
+                        valor,
+                        tipo,
+                        descricao,
+                        cliente_id
+                    )
+                VALUES
+                    (   
+                        $1,
+                        $2,
+                        $3,
+                        $4
+                    )
+            `,
+            [
+                body.valor,
+                body.tipo,
+                body.descricao,
+                params.id,
+            ],
+        );
 
-    res.send();
+        const updateBalance = async (balance, userId) => {
+            await client.query(`UPDATE clientes SET saldo_inicial = $1 where id = $2`, [balance, userId]);
+        }
+
+        const balance = result.rows[0].saldo_inicial - body.valor;
+
+        if (body.tipo === 'c') {
+            await updateBalance(balance, params.id);
+        } else {
+            if (balance < result.rows[0].limite) {
+                return res.status(422).json({
+                    message: 'Num pode'
+                })
+            }
+
+            await updateBalance(balance, params.id);
+        }
+
+        return res.json({
+            'limite': result.rows[0].limite,
+            'saldo': balance
+        });
+    } catch (err) {
+        return res.status(500).send();
+    }
 })
 
+
+app.get('/clientes/:id/extrato', async (req, res) => {
+    try {
+        const result = await client.query(`SELECT * from clientes where id = $1`, [req.params.id]);
+
+        if (!result.rowCount) {
+            return res.status(404).send()
+        }
+
+        const transactions = await client.query('SELECT * FROM transacoes where cliente_id = $1 ORDER BY data_de_criacao DESC LIMIT 10', [req.params.id])
+
+        return res.json({
+                "saldo": {
+                "total": result.rows[0].saldo_inicial,
+                "data_extrato": new Date(),
+                "limite": result.rows[0].limite
+                },
+
+                "ultimas_transacoes": transactions.rows.map((transaction) => ({
+                    "valor": transaction.valor,
+                    "tipo": transaction.tipo,
+                    "descricao": transaction.descricao,
+                    "realizada_em": transaction.data_de_criacao
+                }))
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
+})
+
+
 app.listen(3000, () => {
-    console.log('aa');
+    console.log('Running at: 3000');
 });
